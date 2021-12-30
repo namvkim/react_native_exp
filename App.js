3b309c65805e42cb7889b3025317faf44aea0050
namvkim <sonnamttt@gmail.com>
2021-12-30 11:00:43 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import Caculator from './screen/Caculator';
+import LoginForm from './screen/LoginForm';
+import FlatList from './screen/FlatList';
+import ScrollView from './screen/ScrollView';
+import Navigation from './screen/Navigation';
+
+const YourApp = () => {
+  return (
+    // <Caculator></Caculator>
+    // <LoginForm></LoginForm>
+    // <FlatList></FlatList>
+    // <ScrollView></ScrollView>
+    <Navigation></Navigation>
+  );
+}
+
+export default YourApp;
---
+++ screen/Caculator.js
@@ -0,0 +1,150 @@
+import React, { useState } from 'react';
+import { StyleSheet, TouchableOpacity, ImageBackground, Text, View } from 'react-native';
+
+const YourApp = () => {
+    const image = { uri: "https://wallpaperaccess.com/full/488567.jpg" };
+
+    const [history, setHistory] = useState('');
+    const [result, setResult] = useState('');
+    const arr = ['C', '±', '/', 'x', '7', '8', '9', '─', '4', '5', '6', '+', '1', '2', '3'];
+    const getValue = (char) => setResult(pre => {
+        if (char >= '0' && char <= '9' || char == '+' || char == '─' || char == 'x' || char == '/' || char == '.') {
+            return pre + char;
+        } else if (char == 'C') {
+            return '';
+        } else {
+            return pre;
+        }
+    });
+    const Caculate = () => {
+        setHistory(result);
+        let array = result.split(/([+,─,x,/])/);
+        let ans = parseInt(array[0]);
+        for (let i = 2; i < array.length; i++) {
+            if (array[i] >= '0' && array[i] <= '9') {
+                if (array[i - 1] == '+') {
+                    ans = ans + parseInt(array[i]);
+                } else if (array[i - 1] == '─') {
+                    ans = ans - parseInt(array[i]);
+                } else if (array[i - 1] == 'x') {
+                    ans = ans * parseInt(array[i]);
+                } else {
+                    ans = ans / parseInt(array[i]);
+                }
+            }
+        }
+        setResult(ans);
+    }
+
+    return (
+        <ImageBackground source={image} resizeMode="cover" style={styles.imageBackground}>
+            <View style={styles.container}>
+                <View style={styles.screen}>
+                    <Text style={styles.history}>{history}</Text>
+                    <Text style={styles.result}>{result}</Text>
+                </View>
+                <View style={styles.content}>
+                    {arr.map((item, index) => {
+                        return (
+                            <TouchableOpacity
+                                key={index}
+                                style={styles.button}
+                                onPress={() => getValue(item)}
+                            >
+                                <Text style={styles.text}>{item}</Text>
+                            </TouchableOpacity>
+                        )
+                    })}
+                    <TouchableOpacity
+                        style={styles.buttonEQ}
+                        onPress={Caculate}
+                    >
+                        <Text style={styles.textEQ}>=</Text>
+                    </TouchableOpacity>
+                    <View style={styles.PstView}>
+                        <TouchableOpacity style={{ width: 60 }} />
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => getValue('0')}
+                        >
+                            <Text style={styles.text}>0</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => getValue('.')}
+                        >
+                            <Text style={styles.text}>.</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{ width: 60 }} />
+                    </View>
+                </View>
+            </View >
+        </ImageBackground>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    imageBackground: {
+        flex: 1,
+        padding: 30,
+        justifyContent: 'center'
+    },
+    container: {
+        padding: 20,
+        justifyContent: 'space-between',
+        backgroundColor: 'white',
+    },
+    screen: {
+        paddingVertical: 20,
+    },
+    history: {
+        color: '#636e72',
+        textAlign: 'right',
+        fontSize: 18,
+    },
+    result: {
+        color: '#636e72',
+        textAlign: 'right',
+        fontSize: 55,
+    },
+    content: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+    },
+    button: {
+        backgroundColor: "#f1f2f6",
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#636e72',
+        fontSize: 25,
+    },
+    PstView: {
+        position: 'absolute',
+        bottom: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%'
+    },
+    buttonEQ: {
+        height: 120,
+        width: 60,
+        borderRadius: 30,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        backgroundColor: '#fab1a0',
+    },
+    textEQ: {
+        color: 'white',
+        fontSize: 35,
+        paddingBottom: 15
+    },
+})
+
+export default YourApp;
